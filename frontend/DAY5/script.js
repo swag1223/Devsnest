7afc6df4ebb3ddbdd185299b5f7c5b06d0cafc3d
swaijot <swaijot1201@gmail.com>
2021-06-11 17:23:06 +0530

    used switch case😎

--- frontend/DAY5/script.js
+++ frontend/DAY5/script.js
@@ -3,52 +3,50 @@ window.onload = () => {
     const num2 = document.getElementById("num2")
     
     const result = document.getElementById("result");
-
-    const addbtn = document.getElementById("add");
-    const subbtn = document.getElementById("subtract");
-    const multbtn = document.getElementById("multiply");
-    const divbtn = document.getElementById("divide");
-
-    addbtn.addEventListener("click", () => {
-        if (isNaN(num1.value) || isNaN(num2.value)) {
-            result.innerHTML = "Enter a VALID input !! "
-        }
-        else {
-            result.innerHTML = "Result: " + ( Number(num1.value) + Number(num2.value));
-        }
-    });
-
-    subbtn.addEventListener("click", () => {
-        if (isNaN(num1.value) || isNaN(num2.value)) {
-            result.innerHTML = "Enter a VALID input !! "
-        }
-        else {
-            result.innerHTML = "Result: " + ( Number(num1.value) - Number(num2.value));
-        }
-    })
-    multbtn.addEventListener("click", () => {
-        if (isNaN(num1.value) || isNaN(num2.value)) {
-            result.innerHTML = "Enter a VALID input !! "
-        }
-        else {
-            result.innerHTML = "Result: " + ( Number(num1.value) * Number(num2.value));
-        }
-    });
-
-    divbtn.addEventListener("click", () => {
-        if (isNaN(num1.value) || isNaN(num2.value)) {
-            result.innerHTML = "Enter a VALID input !! "
-        }
-        else {
-            result.innerHTML = "Result: " + ( Number(num1.value) / Number(num2.value));
-        }
-    });
-
-
-
-      
- 
-
+    const buttons = document.querySelector(".buttons-group");
     
+    buttons.addEventListener("click", doTask);
+
+    function doTask(e) {
+        const operator = e.target.innerText;
+        switch (operator) {
+            case "+":
+                if (isNaN(num1.value) || isNaN(num2.value)) {
+                    result.innerHTML = "Enter a VALID input !! ";
+                }
+                else {
+                    result.innerHTML = "Result: " + (Number(num1.value) + Number(num2.value));
+                }
+                break;
+            
+            case "-":
+                if (isNaN(num1.value) || isNaN(num2.value)) {
+                    result.innerHTML = "Enter a VALID input !! ";
+                }
+                else {
+                    result.innerHTML = "Result: " + (Number(num1.value) - Number(num2.value));
+                }
+                break;
+            
+            case "x":
+                if (isNaN(num1.value) || isNaN(num2.value)) {
+                    result.innerHTML = "Enter a VALID input !! ";
+                }
+                else {
+                    result.innerHTML = "Result: " + (Number(num1.value) * Number(num2.value));
+                }
+                break;
+            
+            case "/":
+                if (isNaN(num1.value) || isNaN(num2.value)) {
+                    result.innerHTML = "Enter a VALID input !! ";
+                }
+                else {
+                    result.innerHTML = "Result: " + (Number(num1.value) / Number(num2.value));
+                }
+                break;   
+                
+        }
+    }
     
 }
