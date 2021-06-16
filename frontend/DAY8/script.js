6ed5ebce58511f4bbf7b404f71115b9a7619a405
swaijot <swaijot1201@gmail.com>
2021-06-16 21:51:20 +0530

    Day8 Completed😎😎

--- frontend/DAY8/script.js
+++ frontend/DAY8/script.js
@@ -1,12 +1,21 @@
-function makepixel() {
+const whitePixels = [
+  129, 130, 148, 149, 150, 151, 167, 168, 169, 170, 171, 172, 186, 187, 189,
+  190, 192, 193, 206, 207, 208, 209, 210, 211, 212, 213, 228, 231, 247, 249,
+  250, 252, 266, 268, 271, 273,
+];
+
+function makepixel(i) {
     var pixel = document.createElement("div");
-    pixel.classList.add("red");
+    if (whitePixels.includes(i)) {
+        pixel.classList.add("white");
+    } 
+    else pixel.classList.add("red");
     return pixel;
 }
 
 var container = document.querySelector(".container");
 for (let i = 0; i < 400; i++){
-    container.appendChild(makepixel());
+    container.appendChild(makepixel(i));
 }
 
 container.addEventListener('click', doTask);
