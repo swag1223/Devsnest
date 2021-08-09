7bec98a33b73071160a4bcea017f48818a75b6b0
swag1223 <swaijot1201@gmail.com>
2021-08-10 01:23:03 +0530

    used bootstrap only

---
+++ frontend/DAY25/bootstrap-tailwind/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CardFromBootstrap from './components/Bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+        <CardFromBootstrap/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/DAY25/bootstrap-tailwind/src/components/Bootstrap.jsx
@@ -0,0 +1,62 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+
+function CardFromBootstrap() {
+  return (
+      <>
+        <h1 style={{ margin: "50px" }} >Hi !! I am a Bootstrap Card</h1>
+          
+          
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="https://picsum.photos/200/300" style={{ height: "180px" }} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="https://picsum.photos/201/300" style={{ height: "180px" }}  />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="https://picsum.photos/200/301" style={{ height: "180px" }} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default CardFromBootstrap;
